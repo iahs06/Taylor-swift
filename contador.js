2abd3f069ba32b401d7ec0b6a916798036315161
iahs06 <aih783171@gmail.com>
2024-06-22 16:21:53 -0600

    Update contador.js

--- contador.js
+++ contador.js
@@ -6,5 +6,11 @@ function actualizarContadorVisitas() {
     document.getElementById('visitasNumero').textContent = contador;
 }
 
+// Función para reiniciar el contador de visitas
+function reiniciarContadorVisitas() {
+    localStorage.removeItem('visitas');
+    actualizarContadorVisitas(); // Llama a la función para actualizar el contador después de reiniciar
+}
+
 // Llama a la función al cargar la página
 actualizarContadorVisitas();
