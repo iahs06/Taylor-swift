c6b818f968f0a25e047d959d40e8de5e329d7c41
iahs06 <aih783171@gmail.com>
2024-06-22 16:23:27 -0600

    Update contador.js

--- contador.js
+++ contador.js
@@ -1,16 +1,5 @@
-// Función para obtener y actualizar el contador de visitas
-function actualizarContadorVisitas() {
-    let contador = localStorage.getItem('visitas') || 0;
-    contador++;
-    localStorage.setItem('visitas', contador);
-    document.getElementById('visitasNumero').textContent = contador;
-}
-
-// Función para reiniciar el contador de visitas
-function reiniciarContadorVisitas() {
-    localStorage.removeItem('visitas');
-    actualizarContadorVisitas(); // Llama a la función para actualizar el contador después de reiniciar
-}
-
-// Llama a la función al cargar la página
-actualizarContadorVisitas();
+// Contador de visitas (usando localStorage)
+let visitas = localStorage.getItem('visitas') || 0;
+visitas++;
+localStorage.setItem('visitas', visitas);
+document.getElementById('contador-visitas').textContent = visitas;
